c295eab55d69c88b019efd4415f3a40530884717
Adrian <magic_nairda@hotmail.com>
2024-10-21 00:19:43 -0600

    vitest2

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      provider: 'istanbul', // Proveedor de cobertura necesario
+      provider: 'istanbul', // Añadimos el proveedor de cobertura necesario
       exclude: [
         '**.js',
         '**.ts',
